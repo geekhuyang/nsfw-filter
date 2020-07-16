2419d05412030f592d2c2dcefe63b1db3779a535
Navendu Pottekkat <navendu@localhost.localdomain>
2020-07-16 20:06:15 +0530

    added option features

--- src/content.js
+++ src/content.js
@@ -19,6 +19,11 @@
 // Used for DEBUGGING purposes.
 const DEBUG = 1;
 if (!DEBUG) console.log = () => { };
+let BLUR = 1;
+let REPLACE = 0;
+let WATERMARK = 0;
+
+const WATERMARK_TEXT = "NSFW";
 
 
 // The script is executed when a user scrolls through a website on the tab that is active in the browser.
@@ -44,6 +49,34 @@ function validImage(image) {
   return valid;
 }
 
+function addTextElementToImageNode(imgNode, textContent) {
+  const originalParent = imgNode.parentElement;
+  const container = document.createElement('div');
+  container.style.position = 'relative';
+  container.style.textAlign = 'center';
+  container.style.colore = 'white';
+  const text = document.createElement('div');
+  text.style.position = 'absolute';
+  text.style.top = '90%';
+  text.style.left = '80%';
+  text.style.transform = 'translate(-50%, -50%)';
+  text.style.fontSize = '20px';
+  text.style.fontFamily = 'Courier New, Courier, monospace';
+  text.style.fontWeight = '700';
+  text.style.color = 'white';
+  text.style.lineHeight = '1em';
+  text.style['-webkit-text-fill-color'] = 'white';
+  text.style['-webkit-text-stroke-width'] = '1px';
+  text.style['-webkit-text-stroke-color'] = 'black';
+  // Add the containerNode as a peer to the image, right next to the image.
+  originalParent.insertBefore(container, imgNode);
+  // Move the imageNode to inside the containerNode;
+  container.appendChild(imgNode);
+  // Add the text node right after the image node;
+  container.appendChild(text);
+  text.textContent = textContent;
+}
+
 function analyzeImage(image) {
   /*
   Calls the background script passing it the image URL.
@@ -57,9 +90,23 @@ function analyzeImage(image) {
       If the image should be filtered, it replaces the NSFW 
       image with a random image from https://unsplash.com
       */
-      const replacedImageSrc = "https://source.unsplash.com/random/" + image.width + "x" + image.height;
-      image.src = replacedImageSrc;
-      image.srcset = "";
+      if (REPLACE) {
+        const replacedImageSrc = "https://source.unsplash.com/random/" + image.width + "x" + image.height;
+        image.src = replacedImageSrc;
+        image.srcset = "";
+      }
+      /*
+      It blurs the image.
+      */
+      if (BLUR) {
+        image.style.filter = "blur(20px)";
+      }
+      /*
+      Adds watermark to the image
+      */
+      if (WATERMARK) {
+        addTextElementToImageNode(image, WATERMARK_TEXT)
+      }
       image.dataset.filtered = true; // sets filterd to true so that they are not analyzed again
       image.dataset.isReplaced = true; // sets isReplaced to true so that they are not analyzed again
     }
